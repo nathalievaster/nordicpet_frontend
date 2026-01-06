2be91a5ddf5cdeb3820ee94f06b7bb1ee572b5d0
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-01-06 16:31:50 +0100

    grund för nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { RouterView, RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><RouterLink to="/">Startsida</RouterLink></li>
+      <li><RouterLink to="/products">Produkter</RouterLink></li>
+      <li><RouterLink to="/login">Login</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
