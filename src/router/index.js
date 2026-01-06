8e37a0a438ed7e8af62b638ddf0524329413917f
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-01-06 16:08:16 +0100

    Grundläggande vyer och routerkonfiguration

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Homeview.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue') 
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: () => import('../views/CategoriesView.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/ProductsView.vue')
+    }
+  ],
 })
 
 export default router
